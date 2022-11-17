78f054604840dd7f550ad1fc7ee538317583adf7
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-11-17 13:32:40 +0100

    view confirmation

--- app.py
+++ app.py
@@ -8,10 +8,17 @@ import time as sl
 desired_caps = dict(
     deviceName='Andriod',
     platformName='Android',
-    appPackage='com.amazon.mShop.android.shopping',
-    appActivity='com.amazon.mShop.home.HomeActivity',
+    appPackage='com.android.chrome',
+    appActivity='org.chromium.chrome.browser.ChromeTabbedActivity',
 
 )
 
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
-driver.implicitly_wait(5)
+driver.implicitly_wait(10)
+
+driver.get('http://google.com')
+sl.sleep(2)
+contexts = driver.contexts
+
+for context in contexts:
+    print(context)
